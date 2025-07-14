20b3a1250cc4c62ebe35f62ae8081de02d5b9163
Ioan-Anton Savulescu <ioanantonsavulescu@outlook.com>
2025-07-14 12:43:52 +0300

    Update dependencies and Python version

--- app.py
+++ app.py
@@ -397,4 +397,6 @@ if __name__ == '__main__':
     nltk.download('vader_lexicon')
     nltk.download('stopwords')
     
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    # Run the app with production settings
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
